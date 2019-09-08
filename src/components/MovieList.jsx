18c2c89725441be33e4ddea6ae34c2d53b2ffb44
fahrezialwi <fahrezialwi@gmail.com>
2019-09-09 00:05:37 +0700

    Refactor modal

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,20 +1,90 @@
 import React, { Component } from 'react'
-import MovieDetail from './MovieDetail'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+import axios from 'axios'
 
 class MovieList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            modal: false,
+            poster: '',
+            title: '',
+            released: '',
+            genre: '',
+            director: '',
+            actors: ''
+        }
+    }
+
+    toggle = () => {
+        this.setState(prevState => ({
+            modal: !prevState.modal
+        }))
+    }
+
+    modalOpen = (imdbID) => {
+        axios.get(
+            'http://www.omdbapi.com/',
+            {
+                params: {
+                    apikey: '98e9d12',
+                    i: imdbID
+                }
+            }
+        ).then((res) => {
+            this.setState({
+                poster: res.data.Poster,
+                title: res.data.Title,
+                released: res.data.Released,
+                genre: res.data.Genre,
+                director: res.data.Director,
+                actors: res.data.Actors
+            })
+        })
+
+        this.toggle()   
+    }
+
+    modal = () => {
+        return (
+            <div>
+                <Modal isOpen={this.state.modal} toggle={this.toggle}>
+                    <ModalHeader toggle={this.toggle}>Movie Detail</ModalHeader>
+                    <ModalBody>
+                        <div className="container-fluid">
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <img src={this.state.poster} alt={this.state.title} className="img-fluid"/>
+                                </div>
+                                <div className="col-md-8">
+                                    <ul className="list-group">
+                                        <li className="list-group-item"><h3>{this.state.title}</h3></li>
+                                        <li className="list-group-item">Released : {this.state.released}</li>
+                                        <li className="list-group-item">Genre : {this.state.genre}</li>                 
+                                        <li className="list-group-item">Director : {this.state.director}</li>                 
+                                        <li className="list-group-item">Actors : {this.state.actors}</li>                 
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="secondary" onClick={this.toggle}>Close</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        )
+    }
+
     renderList = () => {
         return this.props.data.map((item) => {
             return (
-                <div className="col-lg-3 col-md-6 col-sm-6 col-12" key={item.imdbID}>
-                    <div className="card card-size mb-3">
+                <div className="col-lg-3 col-md-6 col-sm-6" key={item.imdbID} onClick={() => {this.modalOpen(item.imdbID)}}>
+                    <div className="card card-size mb-3 card-link">
                         <img src={item.Poster} className="card-image" alt={item.Title}/>
                         <div className="card-body">
                         <h5 className="card-title word-break">{item.Title}</h5>
                         <h6 className="card-subtitle mb-2 text-muted">{item.Year}</h6>
-                        <MovieDetail      
-                            imdbID = {item.imdbID}
-                            poster = {item.Poster}
-                        />
                         </div>
                     </div>
                 </div>
@@ -26,6 +96,7 @@ class MovieList extends Component {
         return (
             <div className="row">  
                 {this.renderList()}
+                {this.modal()}
             </div>
         )
     }
