5d4ed6acf7c7a4ccef5a24b8fe1285c6a39ef0f8
fahrezialwi <fahrezialwi@gmail.com>
2019-09-09 23:56:10 +0700

    Redesign modal

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+import { Modal, ModalBody } from 'reactstrap'
 import axios from 'axios'
 
 class MovieList extends Component {
@@ -12,7 +12,12 @@ class MovieList extends Component {
             released: '',
             genre: '',
             director: '',
-            actors: ''
+            actors: '',
+            imdbRating : '',
+            imdbVotes: '',
+            rated: '',
+            runtime: '',
+            plot: ''
         }
     }
 
@@ -38,7 +43,12 @@ class MovieList extends Component {
                 released: res.data.Released,
                 genre: res.data.Genre,
                 director: res.data.Director,
-                actors: res.data.Actors
+                actors: res.data.Actors,
+                imdbRating : res.data.imdbRating,
+                imdbVotes: res.data.imdbVotes,
+                rated: res.data.Rated,
+                runtime: res.data.Runtime,
+                plot: res.data.Plot
             })
         })
 
@@ -49,28 +59,65 @@ class MovieList extends Component {
         return (
             <div>
                 <Modal isOpen={this.state.modal} toggle={this.toggle} centered>
-                    <ModalHeader><div className="container-fluid">Movie Detail</div></ModalHeader>
-                    <ModalBody>
-                        <div className="container-fluid">
-                            <div className="row">
-                                <div className="col-4">
-                                    <img src={this.state.poster} alt={this.state.title} className="img-fluid"/>
+                    <ModalBody className="modal-padding">
+                        <div className="row">
+                            <div className="col-4">
+                                <img src={this.state.poster} alt={this.state.title} className="img-fluid"/>
+                            </div>
+                            <div className="col-8">
+                                <h3>{this.state.title}</h3>
+                                <div className="row">
+                                    <div className="col-8">
+                                        <div className="imdb-rated">{this.state.rated}</div>
+                                        <div className="imdb-runtime">{this.state.runtime}</div>
+                                    </div>
+                                    <div className="col-4 text-right">
+                                        <img className="imdb-star" src="Yellow_Star.png"  alt="rating"/>
+                                        <div className="imdb-rating">{this.state.imdbRating}</div>
+                                        <div className="imdb-rating-divisor">/10</div>
+                                    </div>  
                                 </div>
-                                <div className="col-8">
-                                    <ul className="list-group">
-                                        <li className="list-group-item"><h3>{this.state.title}</h3></li>
-                                        <li className="list-group-item">Released : {this.state.released}</li>
-                                        <li className="list-group-item">Genre : {this.state.genre}</li>                 
-                                        <li className="list-group-item">Director : {this.state.director}</li>                 
-                                        <li className="list-group-item">Actors : {this.state.actors}</li>                 
-                                    </ul>
+                                <div className="row imdb-list">
+                                    <div className="col-12 mt-2">
+                                        <div className="row">
+                                            <div className="col-4 pr-0 text-bold">Release date</div>
+                                            <div className="col-8 pl-0">{this.state.released}</div>
+                                        </div>
+                                    </div>
+                                    <div className="col-12 mt-1">
+                                        <div className="row">
+                                            <div className="col-4 pr-0 text-bold">Genre</div>
+                                            <div className="col-8 pl-0">{this.state.genre}</div>
+                                        </div>
+                                    </div>          
+                                    <div className="col-12 mt-1">
+                                        <div className="row">
+                                            <div className="col-4 pr-0 text-bold">Director</div>
+                                            <div className="col-8 pl-0">{this.state.director}</div>
+                                        </div>
+                                    </div>               
+                                    <div className="col-12 mt-1">
+                                        <div className="row">
+                                            <div className="col-4 pr-0 text-bold">Actors</div>
+                                            <div className="col-8 pl-0">{this.state.actors}</div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <div className="row">
+                            <div className="col-12 mt-4 mb-3">
+                                <div className="card">
+                                    <div className="card-description">{this.state.plot}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-2 text-right ml-auto">
+                                <div className="close-button" onClick={this.toggle}>Close</div>
+                            </div>  
+                        </div>
                     </ModalBody>
-                    <ModalFooter>
-                        <Button color="secondary" onClick={this.toggle}>Close</Button>
-                    </ModalFooter>
                 </Modal>
             </div>
         )
